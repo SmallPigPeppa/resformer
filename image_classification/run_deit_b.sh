ff8d26a776692bf2d442ebce5f4ccd2ce6247268
lwz <905328134@qq.com>
2024-04-22 21:03:20 +0800

    initial

--- image_classification/run_deit_b.sh
+++ image_classification/run_deit_b.sh
@@ -1,3 +1,3 @@
 YOUR_DATA_PATH=/root/autodl-tmp/torch_ds/imagenet
 YOUR_OUTPUT_PATH=/root/autodl-tmp/code/resformer/image_classification/out
-python -m torch.distributed.launch --nproc_per_node 1 main.py --data-path ${YOUR_DATA_PATH} --model resformer_base_patch16 --output_dir ${YOUR_OUTPUT_PATH} --batch-size 128 --pin-mem --input-size 224 160 128 --auto-resume --distillation-type 'smooth-l1' --distillation-target cls --sep-aug --epochs 200 --drop-path 0.2 --lr 8e-4 --warmup-epochs 20 --clip-grad 5.0 --epochs 200 --cooldown-epochs 0
+python -m torch.distributed.launch --nproc_per_node 4 main.py --data-path ${YOUR_DATA_PATH} --model resformer_base_patch16 --output_dir ${YOUR_OUTPUT_PATH} --batch-size 128 --pin-mem --input-size 224 160 128 --auto-resume --distillation-type 'smooth-l1' --distillation-target cls --sep-aug --epochs 200 --drop-path 0.2 --lr 8e-4 --warmup-epochs 20 --clip-grad 5.0 --epochs 200 --cooldown-epochs 0
