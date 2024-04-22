ed4c62d660ec9fdaa0e7304bb51ce84cdd828c44
lwz <905328134@qq.com>
2024-04-22 23:23:14 +0800

    initial

--- image_classification/models.py
+++ image_classification/models.py
@@ -376,7 +376,7 @@ def load_timm_pretrained_weights(model, model_name, checkpoint_path=None):
 
 @register_model
 def resformer_base_patch16_pretrained(pretrained=False, **kwargs):
-    import pdb;pdb.set_trace()
+    # import pdb;pdb.set_trace()
     model = ResFormer(
         patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
