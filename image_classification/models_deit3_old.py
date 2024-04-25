5f3b786e55aef94718970cf76ca5c50dac375bb7
lwz <905328134@qq.com>
2024-04-25 19:54:22 +0800

    initial

--- image_classification/models_deit3_old.py
+++ image_classification/models_deit3_old.py
@@ -28,6 +28,109 @@ from timm.models.registry import register_model
 from typing import Any, Callable, Dict, Optional, Sequence, Set, Tuple, Type, Union, List
 from torch.jit import Final
 
+# class Attention(nn.Module):
+#     def __init__(self, dim, num_heads=8, qkv_bias=False, attn_drop=0., proj_drop=0.):
+#         super().__init__()
+#         self.num_heads = num_heads
+#         head_dim = dim // num_heads
+#         self.scale = head_dim ** -0.5
+#         self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+#         self.attn_drop = nn.Dropout(attn_drop)
+#         self.proj = nn.Linear(dim, dim)
+#         self.proj_drop = nn.Dropout(proj_drop)
+#         self.get_v = nn.Conv2d(head_dim, head_dim, kernel_size=3, stride=1, padding=1, groups=head_dim)
+#         nn.init.zeros_(self.get_v.weight)
+#         nn.init.zeros_(self.get_v.bias)
+#
+#     def get_local_pos_embed(self, x):
+#         B, _, N, C = x.shape
+#         H = W = int(np.sqrt(N - 1))
+#         x = x[:, :, 1:].transpose(-2, -1).contiguous().reshape(B * self.num_heads, -1, H, W)
+#         local_pe = self.get_v(x).reshape(B, -1, C, N - 1).transpose(-2, -1).contiguous()  # B, H, N-1, C
+#         local_pe = torch.cat((torch.zeros((B, self.num_heads, 1, C), device=x.device), local_pe), dim=2)  # B, H, N, C
+#         return local_pe
+#
+#     def forward(self, x):
+#         B, N, C = x.shape
+#         qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+#         q, k, v = qkv.unbind(0)  # make torchscript happy (cannot use tensor as tuple)
+#         attn = (q @ k.transpose(-2, -1)) * self.scale
+#         attn = attn.softmax(dim=-1)
+#         attn = self.attn_drop(attn)
+#         local_pe = self.get_local_pos_embed(v)
+#         x = ((attn @ v + local_pe)).transpose(1, 2).reshape(B, N, C)
+#         x = self.proj(x)
+#         x = self.proj_drop(x)
+#         return x
+
+#
+# class Block(nn.Module):
+#     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, drop=0., attn_drop=0.,
+#                  drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
+#         super().__init__()
+#         self.norm1 = norm_layer(dim)
+#         self.attn = Attention(dim, num_heads=num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
+#         # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+#         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+#         self.norm2 = norm_layer(dim)
+#         mlp_hidden_dim = int(dim * mlp_ratio)
+#         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+#
+#     def forward(self, x):
+#         x = x + self.drop_path(self.attn(self.norm1(x)))
+#         x = x + self.drop_path(self.mlp(self.norm2(x)))
+#         return x
+
+
+class Attention(nn.Module):
+    # fused_attn: Final[bool]
+
+    def __init__(
+            self,
+            dim: int,
+            num_heads: int = 8,
+            qkv_bias: bool = False,
+            qk_norm: bool = False,
+            attn_drop: float = 0.,
+            proj_drop: float = 0.,
+            norm_layer: nn.Module = nn.LayerNorm,
+    ) -> None:
+        super().__init__()
+        assert dim % num_heads == 0, 'dim should be divisible by num_heads'
+        self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+        self.scale = self.head_dim ** -0.5
+        self.fused_attn = False
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.q_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
+        self.k_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv.unbind(0)
+        q, k = self.q_norm(q), self.k_norm(k)
+
+        if self.fused_attn:
+            x = F.scaled_dot_product_attention(
+                q, k, v,
+                dropout_p=self.attn_drop.p if self.training else 0.,
+            )
+        else:
+            q = q * self.scale
+            attn = q @ k.transpose(-2, -1)
+            attn = attn.softmax(dim=-1)
+            attn = self.attn_drop(attn)
+            x = attn @ v
+
+        x = x.transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
 
 
 class LayerScale(nn.Module):
@@ -44,172 +147,51 @@ class LayerScale(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return x.mul_(self.gamma) if self.inplace else x * self.gamma
 
-class Attention(nn.Module):
-    def __init__(self, dim, num_heads=8, qkv_bias=False, attn_drop=0., proj_drop=0.):
-        super().__init__()
-        self.num_heads = num_heads
-        head_dim = dim // num_heads
-        self.scale = head_dim ** -0.5
-        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
-        self.attn_drop = nn.Dropout(attn_drop)
-        self.proj = nn.Linear(dim, dim)
-        self.proj_drop = nn.Dropout(proj_drop)
-        self.get_v = nn.Conv2d(head_dim, head_dim, kernel_size=3, stride=1, padding=1, groups=head_dim)
-        nn.init.zeros_(self.get_v.weight)
-        nn.init.zeros_(self.get_v.bias)
 
-    def get_local_pos_embed(self, x):
-        B, _, N, C = x.shape
-        H = W = int(np.sqrt(N - 1))
-        x = x[:, :, 1:].transpose(-2, -1).contiguous().reshape(B * self.num_heads, -1, H, W)
-        local_pe = self.get_v(x).reshape(B, -1, C, N - 1).transpose(-2, -1).contiguous()  # B, H, N-1, C
-        local_pe = torch.cat((torch.zeros((B, self.num_heads, 1, C), device=x.device), local_pe), dim=2)  # B, H, N, C
-        return local_pe
-
-    def forward(self, x):
-        B, N, C = x.shape
-        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
-        q, k, v = qkv.unbind(0)  # make torchscript happy (cannot use tensor as tuple)
-        attn = (q @ k.transpose(-2, -1)) * self.scale
-        attn = attn.softmax(dim=-1)
-        attn = self.attn_drop(attn)
-        local_pe = self.get_local_pos_embed(v)
-        x = ((attn @ v + local_pe)).transpose(1, 2).reshape(B, N, C)
-        x = self.proj(x)
-        x = self.proj_drop(x)
-        return x
-
-#
 class Block(nn.Module):
-    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, drop=0., attn_drop=0.,init_values=1e-6,
-                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
+    def __init__(
+            self,
+            dim: int,
+            num_heads: int,
+            mlp_ratio: float = 4.,
+            qkv_bias: bool = False,
+            qk_norm: bool = False,
+            proj_drop: float = 0.,
+            attn_drop: float = 0.,
+            init_values: Optional[float] = None,
+            drop_path: float = 0.,
+            act_layer: nn.Module = nn.GELU,
+            norm_layer: nn.Module = nn.LayerNorm,
+            mlp_layer: nn.Module = Mlp,
+    ) -> None:
         super().__init__()
         self.norm1 = norm_layer(dim)
-        self.attn = Attention(dim, num_heads=num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
-        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
-        # self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-        # self.norm2 = norm_layer(dim)
-        # mlp_hidden_dim = int(dim * mlp_ratio)
-        # self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.attn = Attention(
+            dim,
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_norm=qk_norm,
+            attn_drop=attn_drop,
+            proj_drop=proj_drop,
+            norm_layer=norm_layer,
+        )
         self.ls1 = LayerScale(dim, init_values=init_values) if init_values else nn.Identity()
         self.drop_path1 = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
         self.norm2 = norm_layer(dim)
-        mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.mlp = mlp_layer(
+            in_features=dim,
+            hidden_features=int(dim * mlp_ratio),
+            act_layer=act_layer,
+            drop=proj_drop,
+        )
         self.ls2 = LayerScale(dim, init_values=init_values) if init_values else nn.Identity()
         self.drop_path2 = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
-    # def forward(self, x):
-    #     x = x + self.drop_path(self.attn(self.norm1(x)))
-    #     x = x + self.drop_path(self.mlp(self.norm2(x)))
-    #
-    #     return x
-
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = x + self.drop_path1(self.ls1(self.attn(self.norm1(x))))
         x = x + self.drop_path2(self.ls2(self.mlp(self.norm2(x))))
         return x
-
-
-# class Attention(nn.Module):
-#     # fused_attn: Final[bool]
-#
-#     def __init__(
-#             self,
-#             dim: int,
-#             num_heads: int = 8,
-#             qkv_bias: bool = False,
-#             qk_norm: bool = False,
-#             attn_drop: float = 0.,
-#             proj_drop: float = 0.,
-#             norm_layer: nn.Module = nn.LayerNorm,
-#     ) -> None:
-#         super().__init__()
-#         assert dim % num_heads == 0, 'dim should be divisible by num_heads'
-#         self.num_heads = num_heads
-#         self.head_dim = dim // num_heads
-#         self.scale = self.head_dim ** -0.5
-#         self.fused_attn = False
-#
-#         self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
-#         self.q_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
-#         self.k_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
-#         self.attn_drop = nn.Dropout(attn_drop)
-#         self.proj = nn.Linear(dim, dim)
-#         self.proj_drop = nn.Dropout(proj_drop)
-#
-#     def forward(self, x: torch.Tensor) -> torch.Tensor:
-#         B, N, C = x.shape
-#         qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
-#         q, k, v = qkv.unbind(0)
-#         q, k = self.q_norm(q), self.k_norm(k)
-#
-#         if self.fused_attn:
-#             x = F.scaled_dot_product_attention(
-#                 q, k, v,
-#                 dropout_p=self.attn_drop.p if self.training else 0.,
-#             )
-#         else:
-#             q = q * self.scale
-#             attn = q @ k.transpose(-2, -1)
-#             attn = attn.softmax(dim=-1)
-#             attn = self.attn_drop(attn)
-#             x = attn @ v
-#
-#         x = x.transpose(1, 2).reshape(B, N, C)
-#         x = self.proj(x)
-#         x = self.proj_drop(x)
-#         return x
-#
-#
-
-#
-#
-# class Block(nn.Module):
-#     def __init__(
-#             self,
-#             dim: int,
-#             num_heads: int,
-#             mlp_ratio: float = 4.,
-#             qkv_bias: bool = False,
-#             qk_norm: bool = False,
-#             proj_drop: float = 0.,
-#             attn_drop: float = 0.,
-#             init_values: Optional[float] = None,
-#             drop_path: float = 0.,
-#             act_layer: nn.Module = nn.GELU,
-#             norm_layer: nn.Module = nn.LayerNorm,
-#             mlp_layer: nn.Module = Mlp,
-#     ) -> None:
-#         super().__init__()
-#         self.norm1 = norm_layer(dim)
-#         self.attn = Attention(
-#             dim,
-#             num_heads=num_heads,
-#             qkv_bias=qkv_bias,
-#             qk_norm=qk_norm,
-#             attn_drop=attn_drop,
-#             proj_drop=proj_drop,
-#             norm_layer=norm_layer,
-#         )
-#         self.ls1 = LayerScale(dim, init_values=init_values) if init_values else nn.Identity()
-#         self.drop_path1 = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-#
-#         self.norm2 = norm_layer(dim)
-#         self.mlp = mlp_layer(
-#             in_features=dim,
-#             hidden_features=int(dim * mlp_ratio),
-#             act_layer=act_layer,
-#             drop=proj_drop,
-#         )
-#         self.ls2 = LayerScale(dim, init_values=init_values) if init_values else nn.Identity()
-#         self.drop_path2 = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-#
-#     def forward(self, x: torch.Tensor) -> torch.Tensor:
-#         x = x + self.drop_path1(self.ls1(self.attn(self.norm1(x))))
-#         x = x + self.drop_path2(self.ls2(self.mlp(self.norm2(x))))
-#         return x
 
 
 class PatchEmbed(nn.Module):
@@ -319,22 +301,22 @@ class ResFormer(nn.Module):
                 dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias,
                 attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer, act_layer=act_layer)
             for i in range(depth)])
-        # self.blocks = nn.Sequential(*[
-        #     Block(
-        #         dim=embed_dim,
-        #         num_heads=num_heads,
-        #         mlp_ratio=mlp_ratio,
-        #         qkv_bias=qkv_bias,
-        #         qk_norm=False,
-        #         init_values=1e-06,
-        #         proj_drop=0.0,
-        #         attn_drop=attn_drop_rate,
-        #         drop_path=dpr[i],
-        #         norm_layer=norm_layer,
-        #         act_layer=act_layer,
-        #         mlp_layer=Mlp,
-        #     )
-        #     for i in range(depth)])
+        self.blocks = nn.Sequential(*[
+            Block(
+                dim=embed_dim,
+                num_heads=num_heads,
+                mlp_ratio=mlp_ratio,
+                qkv_bias=qkv_bias,
+                qk_norm=False,
+                init_values=1e-06,
+                proj_drop=0.0,
+                attn_drop=attn_drop_rate,
+                drop_path=dpr[i],
+                norm_layer=norm_layer,
+                act_layer=act_layer,
+                mlp_layer=Mlp,
+            )
+            for i in range(depth)])
 
 
         self.norm = norm_layer(embed_dim)
