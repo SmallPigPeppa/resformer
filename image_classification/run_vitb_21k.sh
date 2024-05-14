ea15675db51925363ea03d96a21373954cd478c6
lwz <905328134@qq.com>
2024-05-14 21:29:53 +0800

    initial

--- image_classification/run_vitb_21k.sh
+++ image_classification/run_vitb_21k.sh
@@ -1,3 +1,4 @@
 YOUR_DATA_PATH=/ppio_net0/torch_ds/imagenet
 YOUR_OUTPUT_PATH=/ppio_net0/code/resformer/image_classification/out-vitb-21k
 python -m torch.distributed.launch --nproc_per_node 8 main.py --data-path ${YOUR_DATA_PATH} --model resformer_base_patch16_vitb_21k --output_dir ${YOUR_OUTPUT_PATH} --batch-size 64 --pin-mem --input-size 224 160 128 --auto-resume --distillation-type 'smooth-l1' --distillation-target cls --sep-aug --epochs 200 --drop-path 0.2 --lr 8e-4 --warmup-epochs 20 --clip-grad 5.0 --epochs 200 --cooldown-epochs 0
+python /ppio_net0/code/openapi.py stop ad2a3777c150f32e
